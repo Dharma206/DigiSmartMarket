2970ef9834856cb51e231dbc8cd9172789d18a38
Dharma206 <163315557+Dharma206@users.noreply.github.com>
2024-06-29 14:52:12 +0530

    Modified Admin Market Yard UI and added api

--- digi-smart-market-interface/src/components/admin/AdminYards.jsx
+++ digi-smart-market-interface/src/components/admin/AdminYards.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import {   Container } from "react-bootstrap";
 
-const AdminYards = () => {
- return (
- <div>AdminYards</div>
- )
-}
+import { Badge, Table } from "reactstrap";
+import '../../components/markets/marketListing.scss'
+import { getMarketVendor, getMarkets, getUsers } from "../../serviceApis/loginapi";
 
-export default AdminYards
+const AdminVendors = () => {
+
+  const [marketVendors,setMarketVendors]=useState([])
+
+  const handlefetchUsers = async () => {
+    try {
+      const userData = await getMarkets();
+      setMarketVendors(userData?.rows||[]);
+      console.log("userData->", userData);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  useEffect(() => {
+
+    handlefetchUsers();
+  }, []);
+
+  return (
+    <Container className="item-list">
+     
+        <div className="d-flex justify-content-between mt-5 mb-3 mt"><div><h4 style={{fontWeight:700}}>Market Yards</h4></div><div>  </div></div>
+        
+      <Table hover>
+          <thead>
+            <tr>
+            <th>Market Name </th>
+            <th>Location </th>
+            <th>Type</th>
+
+
+            </tr>
+          </thead>
+          <tbody>
+        { marketVendors?.length === 0 ? (
+              <tr>
+                <td colSpan="9" className="no-records-found">
+                  No records found
+                </td>
+              </tr>
+            ) : (
+              marketVendors?.map((item) => (
+                <tr key={item?.id}>
+                  <td>{item?.marketName||'-'}</td>
+                  <td>{item?.location||'-'}</td>
+           <td><Badge color="info" pill rounded>Market</Badge></td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </Table>
+
+    </Container>
+  );
+};
+
+export default AdminVendors;
